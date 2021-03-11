8d9ed25df1ad7cd87bd72c74710cd24bed93db28
trejgun <trejgun@gmail.com>
2021-03-11 20:40:43 +0800

    fix webpack/babel plugins add renovate update eslint config make table rows actually sortable

--- client/pages/content/index.tsx
+++ client/pages/content/index.tsx
@@ -17,12 +17,30 @@ export interface IResultsSelectorRef {
   addRow: (row: Array<string>) => void;
 }
 
-const headCells = [
-  {id: "id", sortable: true, label: "ID"},
-  {id: "slot1", sortable: false, label: "Slot 1"},
-  {id: "slot2", sortable: false, label: "Slot 2"},
-  {id: "slot3", sortable: false, label: "Slot 3"},
-  {id: "timestamp", sortable: true, label: "Date"},
+export enum Column {
+  id = "id",
+  slot1 = "slot1",
+  slot2 = "slot2",
+  slot3 = "slot3",
+  timestamp = "timestamp",
+}
+
+export type IRow = {
+  [key in keyof typeof Column]: string | number;
+};
+
+export interface IHeadCell {
+  id: string;
+  sortable: boolean;
+  label: string;
+}
+
+const headCells: Array<IHeadCell> = [
+  {id: Column.id, sortable: true, label: "ID"},
+  {id: Column.slot1, sortable: false, label: "Slot 1"},
+  {id: Column.slot2, sortable: false, label: "Slot 2"},
+  {id: Column.slot3, sortable: false, label: "Slot 3"},
+  {id: Column.timestamp, sortable: true, label: "Date"},
 ];
 
 export enum Direction {
@@ -31,7 +49,7 @@ export enum Direction {
 }
 
 export const Results = forwardRef<IResultsSelectorRef, IResultsSelectorProps>((_props, ref) => {
-  const [rows, setRows] = useState<Array<any>>([]);
+  const [rows, setRows] = useState<Array<IRow>>([]);
   const [order, setOrder] = useState<Direction>(Direction.asc);
   const [orderBy, setOrderBy] = useState("id");
 
@@ -41,10 +59,40 @@ export const Results = forwardRef<IResultsSelectorRef, IResultsSelectorProps>((_
     },
   }));
 
-  const createSortHandler = (property: string) => () => {
+  const handleRequestSort = (property: string) => {
     const isAsc = orderBy === property && order === Direction.asc;
     setOrder(isAsc ? Direction.desc : Direction.asc);
     setOrderBy(property);
+  };
+
+  const descendingComparator = (a: any, b: any, orderBy: string) => {
+    if (b[orderBy] < a[orderBy]) {
+      return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+      return 1;
+    }
+    return 0;
+  };
+
+  const getComparator = (order: string, orderBy: string) => {
+    return order === "desc"
+      ? (a: any, b: any) => descendingComparator(a, b, orderBy)
+      : (a: any, b: any) => -descendingComparator(a, b, orderBy);
+  };
+
+  const stableSort = (array: Array<IRow>, comparator: (a: any, b: any) => number) => {
+    const stabilizedThis: Array<[IRow, number]> = array.map((el, index) => [el, index]);
+    stabilizedThis.sort((a, b) => {
+      const order = comparator(a[0], b[0]);
+      if (order !== 0) return order;
+      return a[1] - b[1];
+    });
+    return stabilizedThis.map(el => el[0]);
+  };
+
+  const createSortHandler = (property: string) => () => {
+    handleRequestSort(property);
   };
 
   return (
@@ -70,7 +118,7 @@ export const Results = forwardRef<IResultsSelectorRef, IResultsSelectorProps>((_
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map(row => (
+          {stableSort(rows, getComparator(order, orderBy)).map(row => (
             <TableRow key={row.id}>
               <TableCell>{row.id}</TableCell>
               <TableCell>{row.slot1}</TableCell>
